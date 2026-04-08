0e858ccd686b813d08fddd90faff086a01a75de9
rlaehgns <ehgns4936@naver.com>
2026-04-09 08:26:47 +0900

    aivle

---
+++ Aivle/나머지.java
@@ -0,0 +1,2 @@
+import java.util.Scanner;
+public class 나머지 { public static void main(String[] args) {int n = new Scanner(System.in).nextInt();System.out.println(n%3);}}
---
+++ Aivle/한우.java
@@ -0,0 +1,4 @@
+import java.util.Scanner;
+
+public class 한우 {
+    public static void main(String[] args) {int n = new Scanner(System.in).nextInt();System.out.println(n>=200?"A":n>=180?"B":n>=150?"C":"D");}}
